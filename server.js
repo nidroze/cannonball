5e610f2863b973beca44545c6d3c65254900eab5
nidroze <cridipuorto@gmail.com>
2018-10-07 13:13:16 +0200

    errori vari guarda differenza tra i commit se ti interessa

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var index = require(',/routes/index');
-var index = require(',/routes/tasks');
+var index = require('./routes/index');
+var tasks = require('./routes/tasks');
 
 var app = express();
 
@@ -25,5 +25,5 @@ app.use('/', index);
 app.use('/api', tasks);
 
 app.listen(port, function() {
-	console.log('Server started on port'+port+' ');
+	console.log('Server started on port '+port+' ');
 });
